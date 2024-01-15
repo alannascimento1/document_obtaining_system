1a9a4307355bc5433ca867398d0fad715142b3bd
alannascimento1 <nascimentoalankst@gmail.com>
2024-01-15 19:13:42 -0300

    config: configura flowbite

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,5 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "flowbite-datepicker", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/datepicker.turbo.min.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
