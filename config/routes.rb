aa5393a71fbbb5d0d9443cae772dd2e8ea3fb25d
alannascimento1 <nascimentoalankst@gmail.com>
2024-01-16 21:15:07 -0300

    feat: adiciona rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users
+  resources :sectors
+  resources :institutions
+  resources :document_types
+  resources :state_transition_documents
+
   root 'pages#index'
   get 'pages/index'
 end
