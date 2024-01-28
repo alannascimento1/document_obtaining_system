92be1d24774bdcfca42e79eda6af3cc7574774da
alannascimento1 <nascimentoalankst@gmail.com>
2024-01-28 16:06:12 -0300

    feat: adiciona controllers de modal

---
+++ app/javascript/controllers/open_modal_controller.js
@@ -0,0 +1,37 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["modal"];
+
+  connect() {
+    this.show()
+  }
+
+  initialize() {
+    const options = {
+      placement: "center",
+      backdropClasses: "bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40",
+      onShow: () => {
+        document.body.classList.add("overflow-y-hidden");
+      },
+      onHide: () => {
+        document.body.classList.remove("overflow-y-hidden");
+        this.dispatch("hide");
+      }
+    };
+
+    this.modal = new Modal(this.modalTarget, options);
+
+    if (this.showValue) {
+      this.show();
+    }
+  }
+
+  show() {
+    this.modal.show();
+  }
+
+  hide() {
+    this.modal.hide();
+  }
+}
