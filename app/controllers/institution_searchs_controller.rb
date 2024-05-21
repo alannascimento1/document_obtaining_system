9eb0db57f28915f62fd1e1f37ea1c96ccf76403f
alannascimento1 <nascimentoalankst@gmail.com>
2024-05-21 15:59:05 -0300

    fix: Remove dependencia da gem pagy

--- app/controllers/institution_searchs_controller.rb
+++ app/controllers/institution_searchs_controller.rb
@@ -4,10 +4,9 @@ class InstitutionSearchsController < ApplicationController
   layout 'base'
 
   def index
-    Pagy::DEFAULT[:items] = 5
     @institutions = Institution.where('name LIKE ?', "%#{params[:search]}%")
+    @records = @institutions.paginate(page: params[:page])
 
-    @pagy, @records = pagy(@institutions)
     @document_records = DocumentRecord.all
 
     redirect_to(pages_index_path(page: params[:page], search: params[:search]))
