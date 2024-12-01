50fd51eead7bc6098949f8a4d69301cf4bfbcc71
alannascimento1 <nascimentoalankst@gmail.com>
2024-12-01 11:29:56 -0300

    style: Faz ajustes no mobile

--- app/javascript/controllers/open_modal_by_click_controller.js
+++ app/javascript/controllers/open_modal_by_click_controller.js
@@ -4,7 +4,6 @@ export default class extends Controller {
   static targets = ["modal"];
 
   initialize() {
-    console.log("initialize");
     const options = {
       placement: "center",
       backdropClasses: "bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40",
@@ -25,7 +24,6 @@ export default class extends Controller {
   }
 
   show() {
-    console.log("show")
     this.modal.show();
   }
 
